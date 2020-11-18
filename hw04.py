cfc5b17ba5b9ab1e58059bd8c9ee8103864494a3
ZihanLiao <54339989+ZihanLiao@users.noreply.github.com>
2020-11-18 18:33:36 -0500

    Add files via upload

--- hw04.py
+++ hw04.py
@@ -6,35 +6,37 @@ import pandas as pd
 from tensorflow.python.keras.engine import data_adapter
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-# def generate_dataset(n_train,filepath):
-#     n_abnormal = np.random.randint(1,15)
-#     T_SET = [];F_SET = []
-#     for _ in range(n_train):
-#         # Generate True set
-#         t_idx = np.random.choice(79,20)
-#         f_idx = np.random.choice(78,20)
-#         t_sample = np.zeros(80)
-#         f_sample = np.random.randint(2,size = 80)
-#         for i in t_idx:
-#             t_sample[i] = 1
-#         T_SET.append(t_sample)
-#         # Generate false set
-#         for i in f_idx:
-#             f_sample[i],f_sample[i+1] = 1,1
-#         F_SET.append(f_sample)
-#     T_set_filepath = filepath + "/t_set.csv"
-#     f = open(T_set_filepath,'w')
-#     writer = csv.writer(f)
-#     for r in range(len(T_SET[:])):
-#         writer.writerow(T_SET[r])
-#     F_set_filepath = filepath + "/f_set.csv"
-#     f = open(F_set_filepath,'w')
-#     writer = csv.writer(f)
-#     for r in range(len(F_SET[:])):
-#         writer.writerow(F_SET[r])
-#     return 
+from sklearn.metrics import confusion_matrix
+import itertools
+def generate_dataset(n_train,filepath):
+    n_abnormal = np.random.randint(1,15)
+    T_SET = [];F_SET = []
+    for _ in range(n_train):
+        # Generate True set
+        t_idx = np.random.choice(79,20)
+        f_idx = np.random.choice(78,20)
+        t_sample = np.zeros(80)
+        f_sample = np.random.randint(2,size = 80)
+        for i in t_idx:
+            t_sample[i] = 1
+        T_SET.append(t_sample)
+        # Generate false set
+        for i in f_idx:
+            f_sample[i],f_sample[i+1] = 1,1
+        F_SET.append(f_sample)
+    T_set_filepath = filepath + "/t_set.csv"
+    f = open(T_set_filepath,'w')
+    writer = csv.writer(f)
+    for r in range(len(T_SET[:])):
+        writer.writerow(T_SET[r])
+    F_set_filepath = filepath + "/f_set.csv"
+    f = open(F_set_filepath,'w')
+    writer = csv.writer(f)
+    for r in range(len(F_SET[:])):
+        writer.writerow(F_SET[r])
+    return 
 FILE_PATH = "/workspace/DeepLearning/hw04"
-# generate_dataset(50,FILE_PATH)
+generate_dataset(500,FILE_PATH)
 
 t_dset = pd.read_csv(FILE_PATH+"/t_set.csv",skiprows = 1)
 f_dset = pd.read_csv(FILE_PATH+"/f_set.csv",skiprows = 1)
@@ -49,31 +51,7 @@ test_data = test_dset.values[:,:80]
 test_data = np.expand_dims(test_data,axis=2)
 test_data = np.array(test_data,dtype='float32')
 test_label = np.array(test_label,dtype='float32')
-# train_loss = tf.keras.metrics.Mean()
-# train_accuracy = tf.keras.metrics.BinaryAccuracy()
 
-# test_loss = tf.keras.metrics.Mean()
-# test_accuracy = tf.keras.metrics.BinaryAccuracy()
-
-# loss_object = tf.keras.metrics.BinaryCrossentropy()
-# optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)
-
-def train_step(dset,label):
-    with tf.GradientTape() as tape:
-        predictions = tdnn(dset,training=True)
-        loss = loss_object(label,predictions)
-    gradients = tape.gradient(loss,tdnn.trainable_variables)
-    optimizer.apply_gradients(zip(gradients,tdnn.trainable_variables))
-
-    train_loss(loss)
-    train_accuracy(label,predictions)
-
-def test_step(dset,label):
-    predictions = tdnn(dset,training=False)
-    t_loss = loss_object(label,predictions)
-
-    test_loss(t_loss)
-    test_accuracy(label,predictions)
 
 def flatten(dset,winsize):
     n_sample,l_sample = np.shape(dset)
@@ -90,20 +68,25 @@ def flatten(dset,winsize):
     
 EPOCHS = 100
 X_train,X_val,y_train,y_val = train_test_split(dset,label,test_size = 0.2,random_state = 42)
-winsize = 20
+winsize = 7
 
 # X_train = flatten(X_train,winsize);X_val = flatten(X_val,winsize)
 X_train = np.expand_dims(X_train,axis=2); X_val = np.expand_dims(X_val,axis=2)
-tdnn = TDNN(k_size = winsize,units1 = 10, units2 = 5)
+tdnn = TDNN(k_size = winsize,units1 = 20, units2 = 20)
 tdnn.compile(optimizer = 'Adam',
+
         loss = 'binary_crossentropy',
         metrics=['accuracy'])
-history = tdnn.fit(X_train,y_train,epochs=500,validation_split=0.2)
+history = tdnn.fit(X_train,y_train,epochs=10,validation_split=0.2)
+predictions = tdnn.predict(test_data)
+predictions = np.where(predictions>0.5,1,0)
 tdnn.evaluate(test_data,test_label)
+cm = confusion_matrix(test_label,predictions)
 loss = history.history['loss']
 acc = history.history['accuracy']
 val_loss = history.history['val_loss']
 val_acc = history.history['val_accuracy']
+plt.figure(1)
 plt.subplot(121)
 plt.plot(loss,'b-',label='loss')
 plt.plot(acc,'r-',label='accuracy')
@@ -112,28 +95,31 @@ plt.subplot(122)
 plt.plot(val_loss,'b-',label='validation loss')
 plt.plot(val_acc,'r-',label='validation accuracy')
 plt.legend()
-plt.show()
-# train_ds = tf.data.Dataset.from_tensor_slices((X_train,y_train)).shuffle(10000).batch(1)
-# val_ds = tf.data.Dataset.from_tensor_slices((X_val, y_val)).batch(1)
-# for epoch in range(EPOCHS):
-#     train_loss.reset_states()
-#     train_accuracy.reset_states()
-#     test_loss.reset_states()
-#     test_accuracy.reset_states()
-#     i = 0
-#     for d,l in train_ds:
-        
-#         train_step(d,l)
-       
-#     for d,l in test_ds:
-#         test_step(d,l)
 
-#     print(
-#     f'Epoch {epoch + 1}, '
-#     f'Loss: {train_loss.result()}, '
-#     f'Accuracy: {train_accuracy.result() * 100}, '
-#     f'Test Loss: {test_loss.result()}, '
-#     f'Test Accuracy: {test_accuracy.result() * 100}'
-#   )
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Reds): 
+    if normalize: 
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis] 
+        print("Normalized confusion matrix") 
+    else: 
+        print('Confusion matrix, without normalization') 
+        # print(cm) 
+    plt.imshow(cm, interpolation='nearest', cmap = 'Blues') 
+    plt.title(title)
+    tick_marks = np.arange(len(classes)) 
+    plt.xticks(tick_marks, classes, rotation=45) 
+    plt.yticks(tick_marks, classes) 
+    fmt = '.2f' if normalize else 'd' 
+    thresh = cm.max() / 2. 
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])): 
+        plt.text(j, i, format(cm[i, j], fmt), 
+            horizontalalignment="center", 
+            color="white" if cm[i, j] > thresh else "black") 
+    plt.tight_layout() 
+    plt.ylabel('True label') 
+    plt.xlabel('Predicted label')
+    plt.show()
+plt.figure(2)
+plot_confusion_matrix(cm,[0,1],normalize=True,title='confusion matrix(tdnn)')
+
     
 
--- tdnn.py
+++ tdnn.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Dense,Conv1D,Flatten,ReLU
+from tensorflow.keras.layers import Dense,Conv1D,Flatten,ReLU,Dropout,BatchNormalization
 from tensorflow.keras import Model
 
 class TDNN(Model):
@@ -7,14 +7,24 @@ class TDNN(Model):
         super(TDNN,self).__init__()
         self.flat = Flatten()
         self.relu = ReLU()
+        self.bn = BatchNormalization()
+        self.dropout = Dropout(0.5)
         self.conv1 = Conv1D(units1,k_size)
         self.conv2 = Conv1D(units2,k_size,)
-        self.dense = Dense(1,activation='sigmoid')
+        self.dense1 = Dense(124,activation='relu')
+        self.dense2 = Dense(1,activation='sigmoid')
     def call(self,X):
         X = self.conv1(X)
+        # X = self.bn(X)
+        X = self.dropout(X)
         X = self.relu(X)
+        
         X = self.conv2(X)
+        X = self.dropout(X)
         X = self.relu(X)
+        
         X = self.flat(X)
-        X = self.dense(X)
+        X = self.dense1(X)
+        X = self.dropout(X)
+        X = self.dense2(X)
         return X
